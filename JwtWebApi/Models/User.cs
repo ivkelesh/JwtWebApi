da65a70749b866977ae8f33560e552cfbb57f2a2
Ivan Cheles <ivan.cheles@iis.utm.md>
2022-03-16 21:49:30 +0200

    Add project files.

---
+++ JwtWebApi/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace JwtWebApi.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        
+        public string Email { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+
+    }
+}
---
+++ JwtWebApi/Models/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace JwtWebApi.Models
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty ;
+        public string Password { get; set; } = string.Empty ;
+        public string Email { get; set; } = string.Empty ;
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+
+    }
+}
